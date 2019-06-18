65d5f52e06960fb3921f31ecd5642244cc7a880d
istvankolumban <kolumbanistvan93@gmail.com>
2019-06-18 11:10:35 +0300

    first commit

---
+++ laracast-5-separate-php-logic-from-html/laracast-5-view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Document</title>
+	<style type="text/css">
+		header {
+			background: gray;
+			padding: 2em;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<header>
+		<h1>
+			<?=$greeting;?>
+		</h1>	
+	</header>
+</body>
+</html>
---
+++ laracast-5-separate-php-logic-from-html/laracast-5.php
@@ -0,0 +1,4 @@
+<?php
+$greeting='This string comes from laracast-5.php.';
+require 'laracast-5-view.php';
+?>
